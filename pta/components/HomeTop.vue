227d812b46060f55fc41d72ddf898b66f0ee19e8
richie <113203265+R1chi333@users.noreply.github.com>
2023-07-06 10:20:29 -0400

    back

---
+++ pta/components/HomeTop.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="homeTopCon">
+        <h1>STATEN ISLAND TECHNICAL HIGH SCHOOL</h1>
+        <h2>Parent Teacher Association</h2>
+    </div>
+</template>
+<style>
+body, h1, h2, h3, p{
+    margin: 0%;
+    padding: 0;
+}
+#homeTopCon{
+    background: linear-gradient(180deg, #FFC498 0%, #FFD88C 28.65%, #FFD88C 61.98%, #FCE5AC 81.25%, #FFF 98.95%, #FFF 98.96%);
+    width: 100vw;
+    height: 50vw;
+}
+</style>
--- pta/pages/index.vue
+++ pta/pages/index.vue
@@ -1,1 +1,12 @@
+<<<<<<< Updated upstream
 <template><h1>Landing Page</h1></template>
+=======
+<script>
+import HomeTop from '../components/HomeTop.vue';
+</script>
+<template>
+    <div>
+        <HomeTop/>
+    </div>
+</template>
+>>>>>>> Stashed changes
