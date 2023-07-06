be9d3634a1fc360cde3086a8291e1eff6f756b0c
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2023-07-06 11:51:12 -0400

    pta logo in place

--- pta/components/Navbar.vue
+++ pta/components/Navbar.vue
@@ -1,55 +1,71 @@
 <template>
   <div id="navbar">
-    <div id="home">
-      <h4><NuxtLink to="/">Home</NuxtLink></h4>
+    <div class="link" id="home">
+      <p>
+        <NuxtLink to="/">Home</NuxtLink>
+      </p>
     </div>
-    <div id="events">
-      <h4><NuxtLink to="/Events">Events</NuxtLink></h4>
+    <div class="link" id="events">
+      <p>
+        <NuxtLink to="/Events">Events</NuxtLink>
+      </p>
     </div>
-    <div id="contact">
-      <h4><NuxtLink to="/ContactUs">Contact Us</NuxtLink></h4>
+    <div class="link" id="contact">
+      <img src="../assets/NavBar-Icons/envelope.svg" alt="" />
+      <p>
+        <NuxtLink to="/ContactUs">Contact Us</NuxtLink>
+      </p>
     </div>
-    <div id="donate">
-      <h4><NuxtLink to="/Donate">Donate</NuxtLink></h4>
+    <div class="link" id="donate">
+      <p>
+        <NuxtLink to="/Donate">Donate</NuxtLink>
+      </p>
     </div>
   </div>
 </template>
 
 <style scoped>
+@import url(../assets/base.css);
+
 #navbar {
   display: flex;
   flex-shrink: 0;
   justify-content: flex-end;
+  color: var(--text-color);
+  font-family: var(--font-nav);
+  font-size: 32px;
+  font-weight: 400;
+  letter-spacing: 0.64px;
+  background: no-repeat url(../assets/NavBar-Icons/pta-logo.png);
 }
-h4 {
+
+a {
   display: flex;
-  width: 93px;
-  height: 27px;
+  width: 163.834px;
+  height: 40.966px;
   flex-direction: column;
   flex-shrink: 0;
+  text-decoration: none;
 }
-#home {
+
+a:visited {
+  color: var(--text-color);
+}
+img {
   display: inline-flex;
-  width: 116px;
-  height: 27px;
+  width: 27.041px;
+  height: 25.793px;
   flex-shrink: 0;
 }
 #events {
-  display: inline-flex;
-  width: 116px;
-  height: 27px;
+  width: 189.057px;
+  height: 40.966px;
   flex-shrink: 0;
 }
-#contact {
+.link {
   display: inline-flex;
-  width: 116px;
-  height: 27px;
-  flex-shrink: 0;
-}
-#donate {
-  display: inline-flex;
-  width: 116px;
-  height: 27px;
+  justify-content: space-evenly;
+  padding: 0px 77px;
   flex-shrink: 0;
 }
 </style>
