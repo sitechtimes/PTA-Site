dc229a7fe4e26f34c580b16b9c423019087482e1
Bridget <fbridget0404@gmail.com>
2023-07-06 10:33:57 -0400

    positioning fix

--- pta/components/ScrollTop.vue
+++ pta/components/ScrollTop.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container__topArrow">
-    <a href="#section__landingPage" class="anchor__topArrow">
+    <a href="#section__landingSection" class="anchor__topArrow">
       <img src="/back-to-top-arrow.svg" >
     </a>
   </div>
@@ -11,5 +11,8 @@
   height: 60px;
   width: 60px;
   opacity: 0.7;
+  position: fixed;
+  bottom: 5%;
+  right: 5%;
 }
 </style>
