0ef6fb7d07810228fc0a93de04da27ba025a819e
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2023-07-10 08:40:32 -0400

    fixed media queries

--- pta/components/Navbar.vue
+++ pta/components/Navbar.vue
@@ -5,36 +5,28 @@
         src="../assets/NavBar-Icons/house.svg"
         alt="dark brown icon of a house"
       />
-      <p>
-        <NuxtLink to="/">Home</NuxtLink>
-      </p>
+      <NuxtLink to="/">Home</NuxtLink>
     </div>
     <div class="link" id="events">
       <img
         src="../assets/NavBar-Icons/calendar.svg"
         alt="dark brown icon of a calendar"
       />
-      <p>
-        <NuxtLink to="/Events">Events</NuxtLink>
-      </p>
+      <NuxtLink to="/Events">Events</NuxtLink>
     </div>
     <div class="link" id="contact">
       <img
         src="../assets/NavBar-Icons/envelope.svg"
         alt="dark brown icon of an envelope"
       />
-      <p>
-        <NuxtLink to="/ContactUs">Contact Us</NuxtLink>
-      </p>
+      <NuxtLink to="/ContactUs">Contact Us</NuxtLink>
     </div>
     <div class="link" id="donate">
       <img
         src="../assets/NavBar-Icons/credit_card.svg"
         alt="dark brown icon of a credit card"
       />
-      <p>
-        <NuxtLink to="/Donate">Donate</NuxtLink>
-      </p>
+      <NuxtLink to="/Donate">Donate</NuxtLink>
     </div>
   </div>
 </template>
@@ -58,6 +50,8 @@ a {
   display: flex;
   width: 163.834px;
   height: 40.966px;
+  padding-top: 30px;
+  padding-bottom: 35px;
   flex-direction: column;
   flex-shrink: 0;
   text-decoration: none;
@@ -69,12 +63,12 @@ a:visited {
 }
 
 img {
-  display: flex;
+  display: inline-flex;
   width: 27.041px;
   height: 25.793px;
   flex-shrink: 0;
   padding-top: 45px;
-  padding-right: 3px;
+  padding-right: 5px;
 }
 
 .link {
@@ -82,61 +76,6 @@ img {
   padding-right: 77px;
   flex-shrink: 0;
   justify-content: space-evenly;
-}
-
-@media screen and (max-width: 425px) {
-  #navbar {
-    background-size: 20%;
-    font-size: 15px;
-    justify-content: center;
-  }
-  a {
-    width: 32.7668px;
-    height: 8.1932px;
-    flex-direction: row;
-    white-space: nowrap;
-  }
-  img {
-    width: 30%;
-    height: 30%;
-    padding-top: 22px;
-    padding-right: 0px;
-  }
-  .link {
-    padding-top: 30px;
-    padding-right: 35px;
-    justify-content: space-evenly;
-  }
-  #donate {
-    padding-left: 25px;
-  }
-}
-
-@media screen and (max-width: 768px) {
-  #navbar {
-    background-size: 15%;
-    font-size: 20px;
-    justify-content: flex-end;
-  }
-  a {
-    width: 77px;
-    height: 25px;
-    flex-direction: row;
-    white-space: nowrap;
-  }
-  img {
-    width: 30%;
-    height: 40%;
-    padding-top: 22px;
-    padding-right: 3px;
-  }
-  .link {
-    padding-right: 35px;
-    justify-content: space-evenly;
-  }
-  #donate {
-    padding-left: 30px;
-  }
 }
 
 @media screen and (max-width: 1024px) {
@@ -162,6 +101,90 @@ img {
   }
   #donate {
     padding-left: 30px;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  #navbar {
+    background-size: 15%;
+    font-size: 20px;
+    justify-content: flex-end;
+  }
+  a {
+    width: 77px;
+    height: 9px;
+    flex-direction: row;
+    white-space: nowrap;
+  }
+  img {
+    width: 30%;
+    height: 40%;
+    padding-top: 23px;
+    padding-right: 3px;
+  }
+  .link {
+    padding-top: 3px;
+    padding-bottom: 10px;
+    padding-right: 35px;
+    justify-content: space-evenly;
+  }
+  #donate {
+    padding-left: 30px;
+  }
+}
+
+@media screen and (max-width: 425px) {
+  #navbar {
+    background-size: 20%;
+    font-size: 15px;
+    justify-content: center;
+  }
+  a {
+    width: 25px;
+    height: 9px;
+    padding-top: 20px;
+    flex-direction: row;
+    white-space: nowrap;
+  }
+  img {
+    width: 30%;
+    height: 30%;
+    padding-top: 23px;
+    padding-right: 3px;
+  }
+  .link {
+    padding-top: 30px;
+    padding-right: 30px;
+    justify-content: space-evenly;
+  }
+  #donate {
+    padding-left: 25px;
+  }
+}
+
+@media screen and (max-width: 320px) {
+  #navbar {
+    background-size: 20%;
+    font-size: 13px;
+    justify-content: center;
+  }
+  a {
+    width: 20px;
+    height: 5px;
+    padding-top: 20px;
+    flex-direction: row;
+    white-space: nowrap;
+  }
+  img {
+    width: 25%;
+    height: 25%;
+    padding-top: 23px;
+    padding-right: 0px;
+  }
+  .link {
+    padding-top: 20px;
+    padding-right: 20px;
+    justify-content: space-evenly;
   }
 }
 </style>
