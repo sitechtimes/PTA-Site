43c760d04472663a5ad84251b4e4b459ad9460bb
Bridget <fbridget0404@gmail.com>
2023-07-10 09:48:06 -0400

    Update PhotoGallery.vue

--- pta/components/PhotoGallery.vue
+++ pta/components/PhotoGallery.vue
@@ -1,18 +1,23 @@
 <template>
-  <Carousel :items-to-show="2.5" :wrap-around="true">
-    <Slide v-for="slide in 10" :key="slide">
-      <div class="carousel__item">{{ slide }}</div>
-    </Slide>
+  <section class="container__carousel">
+    <Carousel :items-to-show="2.5" :wrap-around="true" :transition="500">
+      <Slide v-for="(image, index) in images" :key="index">
+        <div class="carousel-item">
+          <img :src="image" class="carousel-image">
+        </div>
+      </Slide>
 
-    <template #addons>
-      <Navigation />
-    </template>
-  </Carousel>
+      <template #addons>
+        <Navigation />
+        <Pagination />
+      </template>
+    </Carousel>
+  </section>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-import { Carousel, Navigation, Slide } from 'vue3-carousel'
+import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel'
 
 import 'vue3-carousel/dist/carousel.css'
 
@@ -21,11 +26,85 @@ export default defineComponent({
   components: {
     Carousel,
     Slide,
+    Pagination,
     Navigation,
   },
+  data() {
+    return {
+      images: [
+        'https://picsum.photos/seed/picsum/200/300',
+        'https://picsum.photos/200/300?grayscale',
+        'https://picsum.photos/200/300/?blur=2'
+        /* insert src's or link to images here
+        format:
+        'image link',
+        'image link',
+        'image link' */
+      ],
+    }
+  }
 })
 </script>
 
 <style scoped>
+.container__carousel {
+  width: 50vw
+}
 
+.carousel-image {
+  border-radius: 1rem;
+}
+
+.carousel__slide {
+  padding: 0.3125;
+}
+
+.carousel__viewport {
+  perspective: 125rem;
+}
+
+.carousel__track {
+  transform-style: preserve-3d;
+}
+
+.carousel__slide--sliding {
+  transition: 0.5s;
+}
+
+.carousel__slide {
+  opacity: 0.9;
+  transform: rotateY(-20deg) scale(0.9);
+}
+
+.carousel__slide--active~.carousel__slide {
+  transform: rotateY(20deg) scale(0.9);
+}
+
+.carousel__slide--prev {
+  opacity: 0.8;
+  transform: rotateY(-10deg) scale(0.95);
+}
+
+.carousel__slide--next {
+  opacity: 0.8;
+  transform: rotateY(10deg) scale(0.95);
+}
+
+.carousel__slide--active {
+  opacity: 1;
+  transform: rotateY(0) scale(1.1);
+  z-index: 100;
+}
+
+.carousel__pagination{
+  margin: 1rem;
+  padding: 0;
+}
+
+@media only screen and (max-width: 480px) {
+
+  .container__carousel {
+    width: 90vw;
+  }
+}
 </style>
