c2c6a35ee39cf0facf5bd8f4518aa760f1e66fc8
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2023-07-11 12:55:55 -0400

    media queering

--- pta/components/ScrollTop.vue
+++ pta/components/ScrollTop.vue
@@ -18,27 +18,32 @@ export default {
 
 <style scoped>
 .container__topArrow {
+  display: flex;
   height: 3rem;
   opacity: 0.7;
   position: fixed;
-  bottom: 5%;
-  right: 3%;
+  bottom: 1.25rem;
+  right: 1.8rem;
   scroll-behavior: smooth;
 }
 
 @media screen and (max-width: 1200px) {
   .container__topArrow {
     height: 3rem;
-    right: 5%;
-    bottom: 5%;
+    bottom: 1.25rem;
+    right: 1.8rem;
+  }
+
+  .container__topArrow img {
+    height: 5rem;
   }
 }
 
 @media screen and (max-width: 992px) {
   .container__topArrow {
     height: 2rem;
-    right: 5%;
-    bottom: 7%;
+    bottom: 5rem;
+    right: 1.8rem;
   }
   .container__topArrow img {
     height: 5rem;
@@ -48,8 +53,8 @@ export default {
 @media screen and (max-width: 576px) {
   .container__topArrow {
     height: 1.5rem;
-    bottom: 5%;
-    right: 5%;
+    bottom: 2.5rem;
+    right: 1rem;
   }
 
   .container__topArrow img {
