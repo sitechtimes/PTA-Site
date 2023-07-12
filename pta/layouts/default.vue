5fca6b364561d1269e2b1fe3df751db101b4ac55
Jessie Yang <90731140+jessiey2@users.noreply.github.com>
2023-07-12 09:39:10 -0400

    layouts again

--- pta/layouts/default.vue
+++ pta/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Navbar />
-    <Nuxt />
+    <slot/>
     <ScrollTop />
   </div>
 </template>
--- pta/pages/Donate.vue
+++ pta/pages/Donate.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-        <Navbar />
-    <ScrollTop />
+
   </div>
 </template>
