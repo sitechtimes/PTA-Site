caba0646badaabb66de21cc962a949d6118f72ab
jessiey2 <90731140+jessiey2@users.noreply.github.com>
2023-07-12 08:03:10 -0400

    added footer to bottom

--- pta/pages/ContactUs.vue
+++ pta/pages/ContactUs.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-        <Navbar />
+    <Navbar />
     <ScrollTop />
+    <Footer />
   </div>
 </template>
--- pta/pages/Events.vue
+++ pta/pages/Events.vue
@@ -2,5 +2,6 @@
   <div>
     <Navbar />
     <ScrollTop />
+    <Footer />
   </div>
 </template>
