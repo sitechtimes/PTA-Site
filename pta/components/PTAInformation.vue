5420e0297743182124bb137ed3190137e2a7807c
Jessie Yang <90731140+jessiey2@users.noreply.github.com>
2023-07-18 09:27:50 -0400

    Squashed commit of the following:
    commit e92516bd5229f57daf9e75d90dfa7cf37aa28b21
    Author: Bridget <113543003+bridgetf7@users.noreply.github.com>
    Date:   Tue Jul 18 09:20:42 2023 -0400
    
        contact page finished; media queries not started/completed (#53)
    
        * commit
    
        * Update ContactUs.vue
    
        * commit
    
        * media queries
    
        * Update ContactUs.vue
    
        * Update ContactUs.vue
    
        * Update ContactUs.vue
    
        * media queries
    
        * iphone media queries
    
        * Update ContactUs.vue
    
        * Update ContactUs.vue
    
        * Update ContactUs.vue
    
        * Update ContactUs.vue
    
        * Update ContactUs.vue
    
        * commit
    
        * progress
    
        * finished; not media queried
    
        * added padding
    
        * Update ContactUs.vue
    
        * Update ContactUs.vue
    
        * Update ContactUs.vue
    
        * Update ContactUs.vue
    
        * gradient
    
        * Update ContactUs.vue
    
        * Update ContactUs.vue
    
        * media query height
    
        * Update ContactUs.vue
    
        * overflow vanquished
    
        * Update ContactUs.vue
    
        * more or less media queried
    
        ipad mini and galaxy fold needs help
    
        * Update ContactUs.vue
    
        * Update ContactUs.vue
    
        * overflow
    
        * margin-left update
    
        ---------
    
        Co-authored-by: richie <113203265+R1chi333@users.noreply.github.com>
        Co-authored-by: YifanZhang4 <bookworm.with.a.problem@gmail.com>

---
+++ pta/components/PTAInformation.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="container__PTAInformation">
+        <div class="container__SocialMedias">
+            <a href=" " target="_blank" rel="noopener">
+                <img src="/SocialMedias/facebook.svg" alt="facebook icon">
+            </a>
+            <a href=" " target="_blank" rel="noopener">
+                <img src="/SocialMedias/instagram.svg" alt="instagram icon">
+            </a>
+            <a href=" " target="_blank" rel="noopener">
+                <img src="/SocialMedias/twitter.svg" alt="twitter icon">
+            </a>
+        </div>
+        <div class="container__TextInformation">
+            <p>
+                sitechpta@gmail.com
+            </p>
+            <p>485 Clawson Street, Staten Island, NY 10306</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+@import url(../assets/base.css);
+
+.container__SocialMedias {
+    display: flex;
+    flex-direction: row;
+    width: 15rem;
+    justify-content: space-evenly;
+    margin-bottom: 0.5rem;
+}
+
+.container__TextInformation {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.container__PTAInformation {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+p {
+    font-family: var(--font-text);
+    color: var(--text-color);
+    text-align: center;
+    font-size: 1.375rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    margin: 0.5rem;
+}
+
+@media only screen and (max-width: 480px) {
+    p{
+        font-size: 1rem;
+    }
+}
+</style>
