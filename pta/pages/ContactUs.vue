e92516bd5229f57daf9e75d90dfa7cf37aa28b21
Bridget <113543003+bridgetf7@users.noreply.github.com>
2023-07-18 09:20:42 -0400

    contact page finished; media queries not started/completed (#53)
    * commit
    
    * Update ContactUs.vue
    
    * commit
    
    * media queries
    
    * Update ContactUs.vue
    
    * Update ContactUs.vue
    
    * Update ContactUs.vue
    
    * media queries
    
    * iphone media queries
    
    * Update ContactUs.vue
    
    * Update ContactUs.vue
    
    * Update ContactUs.vue
    
    * Update ContactUs.vue
    
    * Update ContactUs.vue
    
    * commit
    
    * progress
    
    * finished; not media queried
    
    * added padding
    
    * Update ContactUs.vue
    
    * Update ContactUs.vue
    
    * Update ContactUs.vue
    
    * Update ContactUs.vue
    
    * gradient
    
    * Update ContactUs.vue
    
    * Update ContactUs.vue
    
    * media query height
    
    * Update ContactUs.vue
    
    * overflow vanquished
    
    * Update ContactUs.vue
    
    * more or less media queried
    
    ipad mini and galaxy fold needs help
    
    * Update ContactUs.vue
    
    * Update ContactUs.vue
    
    * overflow
    
    * margin-left update
    
    ---------
    
    Co-authored-by: richie <113203265+R1chi333@users.noreply.github.com>
    Co-authored-by: YifanZhang4 <bookworm.with.a.problem@gmail.com>

--- pta/pages/ContactUs.vue
+++ pta/pages/ContactUs.vue
@@ -1,1 +1,464 @@
-<template></template>
+<template>
+  <div class="container__ContactUs">
+    <div id="gradient">
+      <div class="container">
+        <div class="container__Navigation">
+          <button class="button__navigation" :class="{ active: currentSection === 'GeneralContacts' }"
+            @click="showSection('GeneralContacts')">
+            General Contacts
+          </button>
+          <button class="button__navigation" :class="{ active: currentSection === 'BoardMembers' }"
+            @click="showSection('BoardMembers')">
+            Board Members
+          </button>
+          <button class="button__navigation" :class="{ active: currentSection === 'JoinUs' }"
+            @click="showSection('JoinUs')">
+            Join Us
+          </button>
+        </div>
+
+        <div class="container__Body">
+          <section id="section__GeneralContacts" v-show="currentSection === 'GeneralContacts'">
+            <div class="container__box">
+              <h1>General Contacts</h1>
+              <PTAInformation id="PTAInformation" />
+            </div>
+          </section>
+
+          <section id="section__BoardMembers" v-show="currentSection === 'BoardMembers'">
+            <div class="container__box">
+              <h1>Board Members</h1>
+              <div class="container__BoardMembers">
+                <div class="container__BoardMembers-profile">
+                  <img src="/profile.svg" alt="Profile Picture Filler" />
+                  <h3>John Doe • Position</h3>
+                  <a href="mailto:johndoe@gmail.com" target="_blank" rel="noopener">
+                    johndoe@gmail.com
+                  </a>
+                </div>
+                <div class="container__BoardMembers-profile">
+                  <img src="/profile.svg" alt="Profile Picture Filler" />
+                  <h3>John Doe • Position</h3>
+                  <a href="mailto:johndoe@gmail.com" target="_blank" rel="noopener">
+                    johndoe@gmail.com
+                  </a>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section id="section__JoinUs" v-show="currentSection === 'JoinUs'">
+            <div class="container__box">
+              <h1>Join Us</h1>
+              <div class="container__ParentVolunteer">
+                <h2>PARENT VOLUNTEER OPPORTUNITIES</h2>
+                <p>
+                  If you would like to volunteer at PTA events, please print out
+                  the attached volunteer form and submit to the PTA Room or
+                  email it to
+                  <a href="mailto:sitechpta@gmail.com" target="_blank" rel="noopener">sitechpta@gmail.com</a>.
+                </p>
+                <p>Be part of the SI TECH PTA Grant Committee</p>
+                <ul>
+                  <li>
+                    The grants committee researches and applies for grants to
+                    help fund school programs and activities including
+                    classroom/school upgrades, art, technology, maker-space, and
+                    the library. We reach out to foundations and corporations.
+                  </li>
+                  <li>
+                    Volunteers are needed to research grant opportunities or
+                    help write the grant proposals. No prior experience is
+                    necessary, time commitment is contingent on grant deadlines.
+                  </li>
+                  <li>
+                    Please email
+                    <a href="mailto:SITHSptagrantcommittee@gmail.com" target="_blank"
+                      rel="noopener">SITHSptagrantcommittee@gmail.com</a>
+                    if interested.
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentSection: "GeneralContacts", //the current section by default is the 'General Contacts' section
+    };
+  },
+  methods: {
+    showSection(section) {
+      this.currentSection = section; //the current section by default is the 'General Contacts' section
+    },
+  },
+};
+</script>
+
+<style scoped>
+@import url(../assets/base.css);
+
+/* #gradient {
+  width: 100vw;
+  height: 100vh;
+  padding: 0%;
+  position: absolute;
+  top: 0%;
+  left: 0%;
+  background: linear-gradient(180deg,
+      #ffc498 0%,
+      #ffd88c 20%,
+      #fce5ac 34%,
+      #fbefd7 44%,
+      #fff 55%);
+} */
+
+.container {
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 15vh;
+  height: fit-content;
+  width: 100vw;
+  align-content: center;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  overflow-x: hidden;
+  overflow-y: hidden;
+}
+
+#gradient {
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 0;
+  background: linear-gradient(180deg, #ffd88c 0%, rgba(217, 217, 217, 0) 100%);
+  overflow-x: hidden;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-content: center;
+  align-items: center;
+  overflow-x: hidden;
+  overflow-y: hidden;
+}
+
+.container__ContactUs {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+}
+
+.container__Navigation {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  margin-right: 0;
+  margin-left: 0;
+  width: 70vw;
+  display: flex;
+}
+
+#container_Body {
+  width: 100vw;
+}
+
+.container__box {
+  width: 70vw;
+  height: auto;
+  background-color: #fcf6e9;
+  border-radius: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  justify-content: center;
+  padding: 2rem;
+  padding-bottom: 3rem;
+  margin: 2rem;
+  overflow-x: hidden;
+}
+
+.container__BoardMembers {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  overflow-x: hidden;
+}
+
+.container__BoardMembers-profile {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 2rem;
+  padding: 0;
+  overflow-x: hidden;
+}
+
+.container__BoardMembers-profile p {
+  text-align: center;
+}
+
+.container__BoardMembers-profile img {
+  width: 9rem;
+}
+
+.container__ParentVolunteer {
+  width: 60vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 3rem;
+}
+
+h1 {
+  color: #483221;
+  text-align: center;
+  font-family: var(--font-heading);
+  font-size: 2.5rem;
+  font-style: normal;
+  font-weight: 800;
+  line-height: normal;
+  letter-spacing: 0.05rem;
+  margin: 2rem 1rem;
+}
+
+h2 {
+  color: #483221;
+  font-family: var(--font-heading);
+  font-size: 2rem;
+  font-style: normal;
+  font-weight: 800;
+  line-height: auto;
+  letter-spacing: 0.1rem;
+  margin: 0;
+}
+
+h3 {
+  color: var(--text-color);
+  font-family: var(--font-heading);
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 0;
+  text-align: center;
+}
+
+p,
+li {
+  color: #483221;
+  font-family: var(--font-text);
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 130%;
+}
+
+a {
+  font-family: var(--font-text);
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 2rem;
+}
+
+button {
+  margin: 1rem;
+  padding: 1rem;
+  width: 14vw;
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: auto;
+  font-family: var(--font-heading);
+  color: var(--text-color);
+  border-radius: 1rem;
+  border: none;
+  transition: background-color 0.3s ease;
+  background-color: transparent;
+  cursor: pointer;
+}
+
+button.active {
+  background-color: #fcf6e9;
+}
+
+section {
+  display: flex;
+  flex-wrap: nowrap;
+  align-content: center;
+  justify-content: center;
+}
+
+@media only screen and (min-width: 1030px) {
+  .container__ParentVolunteer{
+  margin-left: 4.5rem;}
+}
+
+@media screen and (max-width: 1200px) {
+  .container {
+    top: 10vh;
+  }
+}
+
+@media screen and (max-width: 992px) {}
+
+@media screen and (max-width: 768px) {}
+
+@media screen and (min-width: 768px) {}
+
+@media screen and (min-width: 768px) and (orientation: landscape) {}
+
+@media screen and (max-width: 576px) {
+  .container__ContactUs {
+    height: 180vh;
+  }
+
+  #gradient {
+    height: 180vh;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+
+  #PTAInformation {
+    margin-top: 0.5rem;
+  }
+
+  .container {
+    top: 15vh;
+  }
+
+  .container__box {
+    display: flex;
+    width: 75vw;
+    border-radius: 1rem;
+    word-wrap: break-word;
+  }
+
+  h1 {
+    font-size: 1.8rem;
+    line-height: 2.5rem;
+    margin: 0.5rem;
+  }
+
+  h2 {
+    color: #483221;
+    font-family: var(--font-heading);
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 800;
+    line-height: auto;
+    letter-spacing: 0.1rem;
+    margin-top: 1.3rem;
+  }
+
+  h3 {
+    font-size: 1rem;
+    line-height: 2rem;
+    margin-bottom: 0.8rem;
+  }
+
+  p,
+  li {
+    color: #483221;
+    font-family: var(--font-text);
+    font-size: 0.8rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 130%;
+  }
+
+  a {
+    font-family: var(--font-text);
+    font-size: 0.8rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 1.5rem;
+  }
+
+  button {
+    font-size: 1rem;
+    border-radius: 1rem;
+    margin-bottom: 0;
+  }
+
+  .container__ContactUs {
+    display: flex;
+    flex-direction: column;
+    height: 160vh;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .container__BoardMembers-profile {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 2rem;
+  }
+
+  .container__BoardMembers-profile img {
+    width: 25vw;
+  }
+}
+
+@media screen and (min-width: 576px) {}
+
+@media screen and (min-width: 576px) and (orientation: landscape) {}
+
+@media only screen and (max-width: 450px) {}
+
+@media only screen and (max-width: 356px) {}
+
+@media screen and (max-height: 1200px) {
+  #gradient {
+    height: 125vh;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+
+  .container__ContactUs {
+    height: 125vh;
+  }
+
+  button {
+    width: 25vw;
+  }
+}
+
+@media screen and (max-height: 992px) {
+  #gradient {
+    height: 110vh;
+  }
+
+  .container__ContactUs {
+    height: 110vh;
+  }
+}
+
+@media screen and (max-height: 768px) {
+  #gradient {
+    height: 140vh;
+  }
+
+  .container__ContactUs {
+    height: 140vh;
+  }
+}
+</style>
