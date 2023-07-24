2201c1113237f28526ae4ff4c18ee4362aab1ad3
vvwenv <90654380+vvwenv@users.noreply.github.com>
2023-07-24 08:51:58 -0400

    slight adjust on delay

--- pta/components/MiddleSection.vue
+++ pta/components/MiddleSection.vue
@@ -38,21 +38,21 @@ export default {
     console.log("mount");
     gsap.from("#activities", {
       scrollTrigger: "#activities",
-      delay: 0.5,
+      delay: 0.2,
       duration: 1,
       x: -600,
       opacity: 0,
     });
     gsap.from("#minutes", {
       scrollTrigger: "#minutes",
-      delay: 0.5,
+      delay: 0.2,
       duration: 1,
       x: -600,
       opacity: 0,
     });
     gsap.from("#gallery", {
       scrollTrigger: "#gallery",
-      delay: 0.5,
+      delay: 0.2,
       duration: 1,
       x: -600,
       opacity: 0,
