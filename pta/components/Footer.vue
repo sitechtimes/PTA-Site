22e980d7c8764d78527bbf4174961d24d45c604e
vvwenv <90654380+vvwenv@users.noreply.github.com>
2023-07-24 08:47:17 -0400

    footer bg css (#118)

--- pta/components/Footer.vue
+++ pta/components/Footer.vue
@@ -50,12 +50,12 @@
   justify-content: center;
   flex-wrap: wrap;
   align-items: center;
-  background: var(--footer-bg);
+  background: white;
   border-radius: 19px;
   width: 50rem;
   padding: 1rem;
   margin-bottom: 3rem;
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  /* box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px; */
 }
 .subtext {
   margin-top: 0.5rem;
