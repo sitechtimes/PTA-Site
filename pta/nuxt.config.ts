1298dd113404434ac3aa03dceee70329b4abf518
Jessie Yang <90731140+jessiey2@users.noreply.github.com>
2023-07-25 11:22:42 -0400

    Revert "help meeeeeeeeeeee"
    This reverts commit 8a751d9619b9e5690e2b91d2091d3a3d5f41f471.

--- pta/nuxt.config.ts
+++ pta/nuxt.config.ts
@@ -1,7 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content'
-  ],
+  devtools: { enabled: true }
 })
- // devtools: { enabled: true },
