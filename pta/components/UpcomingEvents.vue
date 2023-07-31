637c52e84fc79ed67cb9cf8cb720fad0b07e22b8
Jessie Yang <90731140+jessiey2@users.noreply.github.com>
2023-07-31 09:06:11 -0400

    margins

--- pta/components/UpcomingEvents.vue
+++ pta/components/UpcomingEvents.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="upcomingEvents">
-    <h3 class="subh" ref="subh">Upcoming Events</h3>
+    <h3 class="subh" ref="subh"> <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M480 32c0-12.9-7.8-24.6-19.8-29.6s-25.7-2.2-34.9 6.9L381.7 53c-48 48-113.1 75-181 75H192 160 64c-35.3 0-64 28.7-64 64v96c0 35.3 28.7 64 64 64l0 128c0 17.7 14.3 32 32 32h64c17.7 0 32-14.3 32-32V352l8.7 0c67.9 0 133 27 181 75l43.6 43.6c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6V300.4c18.6-8.8 32-32.5 32-60.4s-13.4-51.6-32-60.4V32zm-64 76.7V240 371.3C357.2 317.8 280.5 288 200.7 288H192V192h8.7c79.8 0 156.5-29.8 215.3-83.3z"/></svg> Upcoming Events</h3>
     <ul class="subtext" id="eventsCon">
       <li
         v-for="(event, index) in upcomingEvents"
@@ -69,6 +69,10 @@ export default {
 
 <style scoped>
 @import url(../assets/base.css);
+svg{
+  fill: var(--text-color);
+margin-right: 2%;
+}
 .subh {
   margin-top: 1.5rem;
   width: 32vw;
@@ -268,12 +272,15 @@ li {
 }
 
 @media screen and (max-width: 576px) {
+  svg{
+    margin-right: 1%;
+  }
   ::-webkit-scrollbar {
     width: 0px;
     background: transparent; /* make scrollbar transparent */
   }
   #upcomingEvents {
-    margin-top: 0;
+    margin-top: 3rem;
     width: 75vw;
   }
   .subh {
