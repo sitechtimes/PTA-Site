9ac9c3e1c7d5100c6c41cbb57e803d0a77d2d7a8
Henry Zheng <zhenghenry2@gmail.com>
2023-07-31 12:12:45 -0400

    slug changes

--- pta/pages/_slug.vue
+++ pta/pages/_slug.vue
@@ -1,5 +1,14 @@
 <template>
-  <main>
-    <ContentDoc />
-  </main>
+  <div>
+<nuxt-link :to="{name:`slug`, params:{slug:slug}}">{{title}}</nuxt-link>
+  </div>
 </template>
+
+<script>
+  export default {
+          async aysncData({$content, params}){
+          const Album = await $content(`posts`, params.slug).fetch()
+          return{Album}
+      },
+  }
+</script>
