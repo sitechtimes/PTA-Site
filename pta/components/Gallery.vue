88eb1ea1e0f7dbae97a2e5be3c0d1afd9c12c012
DariaG16 <113132247+DariaG16@users.noreply.github.com>
2023-08-03 10:34:49 -0400

    177 photo gallery cms  (#191)
    * .
    
    * ughhhhhhhhhhhhhh
    
    * lets begin
    
    * NO IMAGE FOR EVENTS >:(
    
    * ok
    
    * I dont get richie's code can they finish this pls
    
    * there u go
    
    * i dont know why the dates are so messed up
    
    * i need to check this
    
    * a
    
    * Revert "Merge branch '167-connecting-events-cms' into 177-photo-gallery-cms"
    
    This reverts commit 199cb8b8bbc6f19ba67968c727a12046b6543c14, reversing
    changes made to 89ee4007719a3ff4dab7e83aaaeeab517918d45b.
    
    * idk
    
    * aaaaaaaaaa
    
    * idk it works but it doesnt
    
    ---------
    
    Co-authored-by: richie <113203265+R1chi333@users.noreply.github.com>

--- pta/components/Gallery.vue
+++ pta/components/Gallery.vue
@@ -1,120 +1,133 @@
 <template>
-    <!-- Slider container -->
-    <div class="slider">
-      <!-- Bind key to the loop index to get unique key for each slide -->
-      <div v-for="(slide, index) in slides" :key="index" class="slide" :style="{ transform: `translateX(${100 * (index - curSlide)}%)` }">
-        <img :src="slide" alt="" />
-      </div>
-  
-      <!-- Control buttons -->
-      <button class="btn btn-next" @click="nextSlide"> <p>►</p></button>
-      <button class="btn btn-prev" @click="prevSlide"><p>◄</p> </button>
+  <!-- Slider container -->
+  <div class="slider">
+    <!-- Bind key to the loop index to get unique key for each slide -->
+    <div
+      v-for="(photo, index) in gallery"
+      :key="index"
+      class="slide"
+      :style="{ transform: `translateX(${100 * (index - curSlide)}%)` }"
+    >
+      <img :src="photo.image" alt="Gallery PTA images" />
     </div>
-  </template>
-  <script>
-  export default {
-    data() {
-      return {
-        slides: [
-          "https://source.unsplash.com/random?landscape,mountain",
-          "https://source.unsplash.com/random?landscape,cars",
-          "https://source.unsplash.com/random?landscape,night",
-          "https://source.unsplash.com/random?landscape,city"
-        ],
-        curSlide: 0
-      };
+    <!-- Control buttons -->
+    <button class="btn btn-next" @click="nextSlide"><p>►</p></button>
+    <button class="btn btn-prev" @click="prevSlide"><p>◄</p></button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      gallery: Array,
+      curSlide: 0,
+    };
+  },
+  methods: {
+    async getGallery() {
+      const query = queryContent("/gallery").find();
+      console.log(query);
+      query.then((response) => {
+        this.gallery = response;
+      });
     },
-    methods: {
-      nextSlide() {
-        // Increment the current slide index, and reset if it reaches the maximum
-        this.curSlide = (this.curSlide + 1) % this.slides.length;
-      },
-      prevSlide() {
-        // Decrement the current slide index, and reset if it becomes negative
-        this.curSlide = (this.curSlide - 1 + this.slides.length) % this.slides.length;
-      }
-    }
-  };
-  </script>
-  <style scoped>
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-  
-  body {
-    height: 100vh;
-    display: grid;
-    place-items: center;
-  }
-  
-  .slider {
-    width: 100%;
-    max-width: 800px;
-    height: 490px;
-    position: relative;
-    overflow: hidden; 
-   
-  }
-  .slide {
-    width: 100%;
-    max-width: 800px;
-    height: 25.5vw;
-    position: absolute;
-    transition: all 0.5s;
-  }
-  
-  .slide img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    border-radius: 15px;
-  }
-  
-  .btn {
-    position: absolute;
-    width: 40px;
-    height: 40px;
-    padding: 10px;
-    border: none;
-    border-radius: 50%;
-    z-index: 10px;
-    cursor: pointer;
-    background-color: #fff;
-    font-size: 18px;
-  }
-  .btn:active {
-    transform: scale(1.1);
-  }
-  .btn-prev {
-    top: 40%;
-    left: 2%;
-  }
-  
-  .btn-next {
-    top: 40%;
-    right: 2%;
-  }
-
-@media screen and (max-width: 1400px) 
-{  .btn-prev, .btn-next{
-    top:39%;
-  }
-  .slider{height: 380px;}
+    nextSlide() {
+      // Increment the current slide index, and reset if it reaches the maximum
+      this.curSlide = (this.curSlide + 1) % this.gallery.length;
+    },
+    prevSlide() {
+      // Decrement the current slide index, and reset if it becomes negative
+      this.curSlide =
+        (this.curSlide - 1 + this.gallery.length) % this.gallery.length;
+    },
+  },
+  mounted() {
+    this.getGallery();
+  },
+};
+</script>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
-@media screen and (max-width: 1200px) {  
-  .slide {}
-  .slider{
+
+body {
+  height: 100vh;
+  display: grid;
+  place-items: center;
+}
+
+.slider {
+  width: 100%;
+  max-width: 800px;
+  height: 490px;
+  position: relative;
+  overflow: hidden;
+}
+.slide {
+  width: 100%;
+  max-width: 800px;
+  height: 25.5vw;
+  position: absolute;
+  transition: all 0.5s;
+}
+
+.slide img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 15px;
+}
+
+.btn {
+  position: absolute;
+  width: 40px;
+  height: 40px;
+  padding: 10px;
+  border: none;
+  border-radius: 50%;
+  z-index: 10px;
+  cursor: pointer;
+  background-color: #fff;
+  font-size: 18px;
+}
+.btn:active {
+  transform: scale(1.1);
+}
+.btn-prev {
+  top: 40%;
+  left: 2%;
+}
+
+.btn-next {
+  top: 40%;
+  right: 2%;
+}
+
+@media screen and (max-width: 1400px) {
+  .btn-prev,
+  .btn-next {
+    top: 39%;
+  }
+  .slider {
+    height: 380px;
+  }
+}
+@media screen and (max-width: 1200px) {
+  .slide {
+  }
+  .slider {
     max-width: 446px;
     height: 317px;
   }
 }
-@media screen and (max-width: 992px) {  
+@media screen and (max-width: 992px) {
   .slide {
     height: 32vw;
   }
-  .slider{   
+  .slider {
     max-width: 600px;
     height: 309px;
   }
@@ -123,44 +136,46 @@
   .slide {
     height: 27vw;
   }
-  .slider{    
+  .slider {
     height: 229px;
   }
 }
-@media screen and (max-width: 768px) {  
+@media screen and (max-width: 768px) {
   .slide {
     height: 30vw;
-  } 
-  .btn-prev, .btn-next{
-    top:39%;
+  }
+  .btn-prev,
+  .btn-next {
+    top: 39%;
   }
 }
-@media screen and (max-width: 576px) {  
+@media screen and (max-width: 576px) {
   .slide {
     height: 40vw;
     margin-top: 2rem;
   }
-  .slider{    
+  .slider {
     height: 232px;
   }
 }
-@media  screen and (max-width: 450px) {  
-  .slide {    
+@media screen and (max-width: 450px) {
+  .slide {
     height: 45vw;
   }
-  .slider{    
+  .slider {
     height: 201px;
   }
 }
-@media  screen and (max-width: 356px) {   
-  .slide {    
+@media screen and (max-width: 356px) {
+  .slide {
     height: 58vw;
   }
-  .slider{    
+  .slider {
     height: 201px;
   }
-  .btn-prev, .btn-next{
-    top:29%;
+  .btn-prev,
+  .btn-next {
+    top: 29%;
   }
 }
 @media screen and (min-width: 576px) {
@@ -171,4 +186,4 @@
 }
 @media screen and (min-width: 768px) and (orientation: landscape) {
 }
-  </style>
+</style>
