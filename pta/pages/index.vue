2d19f1975be85ba7b1cee02d7721a6c05a925098
Jessie Yang <90731140+jessiey2@users.noreply.github.com>
2023-08-09 08:17:13 -0400

    Update index.vue

--- pta/pages/index.vue
+++ pta/pages/index.vue
@@ -24,7 +24,7 @@
 <script setup>
 import { gsap } from "gsap/dist/gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger);
+
 useHead({
   script: [
     { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
