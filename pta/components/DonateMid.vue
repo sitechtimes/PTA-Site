0942fb1dcd575e94fb9c3ac5e03111ca0bf93274
Jessie Yang <90731140+jessiey2@users.noreply.github.com>
2023-09-26 10:26:56 -0400

    dfad

--- pta/components/DonateMid.vue
+++ pta/components/DonateMid.vue
@@ -4,8 +4,7 @@
                 <h3 class="subh" id="sub" ref="subh2">Monetary Donations</h3>
                 <p ref="text3">Monetary donations are always graciously accepted throughout the school year. If you have already donated, we thank you for your generosity and support.</p>
                 <p ref="text4">To donate, please bring the completed form and donation to Ms. Barbara Malenfant’s office, Room 117</p>
-
-                </div>
+</div>
                 
 
 </template>
@@ -18,27 +17,29 @@ export default {
     const { subh2 } = this.$refs;
     const { text3 } = this.$refs;
     const { text4 } = this.$refs;
+
     gsap.from(list, { delay: 1.2, duration: 1, y: 100, opacity: 0 });
     gsap.from(subh2, { delay: 1.4, duration: 1, y: 100, opacity: 0 });
     gsap.from(text3, { delay: 1.6, duration: 1, y: 100, opacity: 0 });
     gsap.from(text4, { delay: 1.8, duration: 1, y: 100, opacity: 0 });
+
   }
 
 };
 </script>
 <style scoped>
 @import url(../assets/base.css);
+
 #monetary{
   margin-top: 15rem;
 }
 .list{
     margin: 1% 0 0 8%;
     font-family: var(  --font-text);
-
 }
 
 p {
-  font-size: 1.7rem;
+font-size: 1.7rem;
 margin: 1% 0 0 8%;
 width: 66%;
 font-family: var(  --font-text);
