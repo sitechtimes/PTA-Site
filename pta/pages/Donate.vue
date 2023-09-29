29965a03ff0527415e2f98b6f5dce832aacba5ed
Jessie Yang <90731140+jessiey2@users.noreply.github.com>
2023-09-29 10:22:52 -0400

    gradient mq

--- pta/pages/Donate.vue
+++ pta/pages/Donate.vue
@@ -67,5 +67,62 @@ export default {
   overflow-x: hidden;
 }
 
+@media screen and (max-width: 576px){
+    #gradient {
+    width: 100%;
+    height: 220vw;
+    margin: 0;
+    padding: 0%;
+    position: absolute;
+    top: 0%;
+    left: 0%;
+  }
+}
 
+@media screen and (min-width: 576px) {
+  #gradient {
+    width: 100%;
+    height: 100vw;
+    margin: 0;
+    padding: 0%;
+    position: absolute;
+    top: 0%;
+    left: 0%;
+  }
+}
+
+
+@media screen and (min-width: 576px) and (orientation: landscape) {
+  #gradient {
+    width: 100%;
+    margin: 0;
+    padding: 0%;
+    position: absolute;
+    top: 0%;
+    left: 0%;
+  }
+}
+
+@media screen and (min-width: 768px) {
+  #gradient {
+    width: 100%;
+    height: 75vw;
+    margin: 0;
+    padding: 0%;
+    position: absolute;
+    top: 0%;
+    left: 0%;
+  }
+}
+
+@media screen and (min-width: 768px) and (orientation: landscape) {
+  #gradient {
+    width: 100%;
+    margin: 0;
+    padding: 0%;
+    position: absolute;
+    top: 0%;
+    left: 0%;
+  }
+}
 </style>
