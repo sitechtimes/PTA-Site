466967a60a624f954066145b520eb2f93b60ef7b
DariaG16 <dariagolitsyna16@gmail.com>
2023-10-11 10:58:40 -0400

    css styling has begun. yay. fun.

--- pta/pages/Gratitude.vue
+++ pta/pages/Gratitude.vue
@@ -1,10 +1,28 @@
 <template>
-<div>
-        <div id="gradient" >
-            <h1 class="heading" ref="heading">Wall of Gratitude</h1>
+  <div>
+    <div id="gradient">
+      <div class="topComponent">
+        <div class="leftComponent">
+          <h1 class="heading" ref="heading">Wall of Gratitude</h1>
+          <div class="thanks">
+            <h2>Thank You!</h2>
+            <p>
+              We would like to thank everyone who helped out Lorem ipsum dolor
+              sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+              ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Ut enim ad minim veniam, quis nostrud exercitation
+              ullamco laboris nis
+            </p>
+          </div>
+        </div>
+        <div class="image"></div>
+        <div class="archive"></div>
+      </div>
     </div>
-</div>
-   
+  </div>
 </template>
 
 <script>
@@ -12,23 +30,19 @@ import { gsap } from "gsap";
 export default {
   mounted() {
     const { heading } = this.$refs;
-    gsap.from(heading, { delay: 0, duration: 1, y: 100, opacity: 0 })
-
-
-  }
-
+    gsap.from(heading, { delay: 0, duration: 1, y: 100, opacity: 0 });
+  },
 };
 </script>
-<style>
-
+<style scoped>
 #gradient {
   width: 100%;
-  height: 90vh;
+  height: 150vh;
   padding: 0%;
   position: absolute;
   top: 0%;
   left: 0%;
-  background:linear-gradient(
+  background: linear-gradient(
     180deg,
     #feb89a 0%,
     #fdd8b8 18%,
@@ -39,12 +53,32 @@ export default {
   );
   overflow-x: hidden;
 }
-
+.topComponent {
+  display: flex;
+  flex-direction: row;
+}
 .heading {
   font-family: "Kumbh Sans", san serif;
   font-weight: 800;
-  font-size: 4rem;
+  font-size: 6rem;
   color: #483221;
   margin: 10% 0 0 8%;
+  width: 33rem;
+  background-color: red;
+}
+.thanks {
+  font-family: "Kumbh Sans", san serif;
+  font-weight: 400;
+  font-size: 1.6rem;
+  color: #483221;
+  margin: 2% 0 0 8%;
+  width: 40rem;
+  background-color: red;
+}
+.image {
+  width: 500px;
+  height: 500px;
+  border-radius: 50%;
+  background-color: red;
 }
 </style>
