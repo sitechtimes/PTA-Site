d2a78cbd69090fb333a0f2eeab1a98a3ab530bde
andreaguo <113132239+andreaguo@users.noreply.github.com>
2024-03-21 10:37:08 -0400

    sadf

--- pta/components/SITHSCalendar.vue
+++ pta/components/SITHSCalendar.vue
@@ -1,22 +1,76 @@
 <template>
     <h1 class="heading">SITHS EVENTS</h1>
     <div class="section">
-        <iframe id="calendar" src="https://calendar.google.com/calendar/embed?height=700&wkst=1&ctz=America%2FNew_York&bgcolor=%23ffffff&src=YmM3MzI4NWRjNGQ2YTQzNjFiZmFmNzZjYjkzMjlkYzMyY2Y2ZDUyODA4M2QxZTdiMmVlNTVjMDNhMzI0ODIxN0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%238E24AA" style="border:solid 1px #777" width="2000" height="900" frameborder="0" scrolling="no"></iframe> </div>
+        <iframe 
+            id="calendar" 
+            class="cal"
+            src="https://calendar.google.com/calendar/embed?height=700&wkst=1&ctz=America%2FNew_York&bgcolor=%23ffffff&src=YmM3MzI4NWRjNGQ2YTQzNjFiZmFmNzZjYjkzMjlkYzMyY2Y2ZDUyODA4M2QxZTdiMmVlNTVjMDNhMzI0ODIxN0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%238E24AA" 
+            style="border:solid 1px #777" 
+            frameborder="0" 
+            scrolling="no">
+        </iframe> 
+        </div>
 </template>
+
+<script>
+import { gsap } from "gsap";
+export default{
+    mounted() {
+    //  const { button } = this.$refs;
+    // gsap.from(".heading", { duration: 1, y: 100, opacity: 0 });
+    // gsap.from(".cal", { duration: 1, y: 100, delay: 0.8, opacity: 0 });
+  },
+}
+</script>
 
 <style lang="css" scoped>
 @import url(../assets/base.css);
-.section{
-    padding-top: 5rem;
-}
+
+
 #calendar{
     display: flex;
     margin: auto;
+    margin-top: 3rem;
+    width: 80vw; 
+    height: 70vh;
 }
 
 .heading{
     text-align: center;
     margin: 0;
-    margin-top: 2rem;
+    margin-top: 4rem;
 }
+@media screen and (max-width: 992px) {
+
+  #calendar {
+    width: 80%;
+    height: 60vw;
+    margin-bottom: 2rem;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  #calendar {
+    width: 70%;
+  }
+  .heading{
+    text-align: center;
+    margin: 0;
+    margin-top: 3rem;
+    margin-bottom: 2rem;
+}
+}
+
+
+
+@media screen and (max-width: 576px) {
+
+  #calendar {
+    width: 80%;
+    height: 60vw;
+    margin-bottom: 2rem;
+  }
+}
+
+
 </style> 
