91cd217996899e2a9bf0a36a91cd499e9ad21eb9
bridgetf7 <fbridget0404@gmail.com>
2025-01-25 22:10:37 -0500

    navigation + footer

--- pta/layouts/default.vue
+++ pta/layouts/default.vue
@@ -1,35 +1,9 @@
 <template>
   <div class="flex flex-col">
-    <Navbar class=""/>
+    <AppLayoutNavBar />
     <slot />
     <ScrollTop />
-    <Footer />
+    <AppLayoutFooter />
   </div>
 </template>
 
-<style>
-/* width */
-::-webkit-scrollbar {
-  width: 15px;
-}
-::-webkit-scrollbar-track {
-  background: linear-gradient(
-    180deg,
-    #ffc498 0%,
-    #fce5ac 20%,
-    #f8e4b2 30%,
-    #fbefd7 75%,
-    #fff 85%
-  );
-}
-/* Handle */
-::-webkit-scrollbar-thumb {
-  background: #483221;
-  border-radius: 15px;
-}
-
-/* Handle on hover */
-::-webkit-scrollbar-thumb:hover {
-  background: #543b28;
-}
-</style>
--- pta/tailwind.config.js
+++ pta/tailwind.config.js
@@ -1,20 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./components/**/*.{js,vue,ts}",
-      "./layouts/**/*.vue",
-      "./pages/**/*.vue",
-      "./plugins/**/*.{js,ts}",
-      "./app.vue",
-      "./error.vue",
-    ],
-    theme: {
-      extend: {
-        colors: {
-          primary: "#483221",
-        }
-
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'light-brown': '#483221',
+        'dark-brown': '#312217',
+        'light-yellow': '#fce5ac',
+      },
+      fontFamily: {
+        karla: ['"Karla"', 'sans-serif'],
       },
     },
-    plugins: [],
-  }
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
