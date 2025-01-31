5bb6ab7aff207b16e54847981b7abeabf9d984a3
katherine <116115224+kaerulean@users.noreply.github.com>
2025-01-31 10:56:14 -0500

    i think i did everything? hopefully? also the menu thing doesn't cover the entire thing maybe needs to fix that too

--- pta/components/Resources.vue
+++ pta/components/Resources.vue
@@ -5,8 +5,8 @@
       <div v-for="resource in resources" :key="resource.title"
         class="bg-white rounded-xl w-full h-full mb-2 flex flex-row border border-[#4c4038a6]">
         <img :src="resource.img" class="p-4 w-1/2 row-span-3 self-center md:w-1/3 xl:w-1/4">
-        <div class="flex flex-col p-4">
-          <h3 class="text-xl md:text-2xl lg:text-4xl font-bold">{{ resource.title }}</h3>
+        <div class="flex flex-col p-4 place-content-center">
+          <h3 class="text-xl md:text-2xl lg:text-3xl font-bold">{{ resource.title }}</h3>
           <p class="text-base md:text-lg lg:text-2xl">{{ resource.description }}</p>
           <a :href="resource.link" target="_blank" rel="noopener noreferrer"
             class="bg-[#C9B23A] rounded-lg px-4 py-2 w-fit md:my-8 my-2 text-center font-semibold text-white transition duration-150 ease-out hover:ease-in hover:opacity-50 text-base md:text-lg lg:text-2xl">
